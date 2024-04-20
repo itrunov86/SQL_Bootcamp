2f76b8ef6e3f521d8176f24574b45d729f656819
matildet <itrunov86@gmail.com>
2024-04-20 09:41:03 +0500

    day03 ex 0 - 2 complite

---
+++ Day3/src/ex02/day03_ex02.sql
@@ -0,0 +1,12 @@
+SELECT m.pizza_name, m.price, pz.name AS pizzeria_name
+FROM
+    menu AS m
+    JOIN pizzeria AS pz ON m.pizzeria_id = pz.id
+    JOIN person_visits AS pv ON pv.pizzeria_id = pz.id
+    JOIN person AS p ON p.id = pv.person_id
+WHERE
+    p.name = 'Kate'
+    AND (
+        price BETWEEN '800' AND '1000'
+    )
+ORDER BY 1, 2, 3
